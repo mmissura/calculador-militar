9303fe7b33ba50514850943514208385f0d04208
Luciano J Guerra <dsibr@hotmail.com>
2025-05-02 14:30:53 -0300

    fix: flowbite version

--- src/components/CustomPopover/index.tsx
+++ src/components/CustomPopover/index.tsx
@@ -3,8 +3,6 @@ type CustomTooltipProps = TooltipProps & {
   children: React.ReactNode;
 };
 
-import './styles.css';
-
 export const CustomTooltip = ({ children, ...rest }: CustomTooltipProps) => {
   return <Tooltip {...rest}>{children}</Tooltip>;
 };
